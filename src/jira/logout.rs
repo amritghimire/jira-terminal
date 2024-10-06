3115b577f95695a4403f2a04930c77ebd7979054
ashupednekar <ashutoshpednekar15@gmail.com>
2024-10-06 21:46:54 +0530

    feat: added logout command

---
+++ src/jira/logout.rs
@@ -0,0 +1,9 @@
+use std::path::Path;
+
+use crate::config::get_config_file_name;
+
+pub fn delete_configuration(){
+    let conf_fn = get_config_file_name();
+    let path = Path::new(&conf_fn);
+    std::fs::remove_file(&path).unwrap(); // user will be prompted for login
+}
