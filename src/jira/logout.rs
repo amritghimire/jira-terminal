c9c0a1a5f97ef598d05f69ce6e8b9c6078c5ffb7
ashupednekar <ashutoshpednekar15@gmail.com>
2024-10-06 21:50:57 +0530

    chore: added success print

--- src/jira/logout.rs
+++ src/jira/logout.rs
@@ -6,4 +6,5 @@ pub fn delete_configuration(){
     let conf_fn = get_config_file_name();
     let path = Path::new(&conf_fn);
     std::fs::remove_file(&path).unwrap(); // user will be prompted for login
+    println!("You've logged out successfully, please keep your jira token handy for next time")
 }
